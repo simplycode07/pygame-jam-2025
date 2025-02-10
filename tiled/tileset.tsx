e1d3b498f1841fce74da8cd8ba30cab86b5b6708
simplycode07 <simplycode07@gmail.com>
2025-02-10 20:39:44 +0530

    tilemaps and levels

---
+++ tiled/tileset.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="basky" tilewidth="32" tileheight="32" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="1"/>
+  </properties>
+  <image source="../../../Downloads/New Piskel.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="-2"/>
+  </properties>
+  <image source="../assets/basky_32x32.png" width="32" height="32"/>
+ </tile>
+</tileset>

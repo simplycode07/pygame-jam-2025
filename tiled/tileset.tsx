ef3da268b3553f9b6b794954ed98d8fab756c23e
simplycode07 <simplycode07@gmail.com>
2025-02-11 22:56:25 +0530

    added new tiles for colors

--- tiled/tileset.tsx
+++ tiled/tileset.tsx
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="basky" tilewidth="32" tileheight="32" tilecount="4" columns="0">
+<tileset version="1.10" tiledversion="1.11.1" name="basky" tilewidth="32" tileheight="32" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
    <property name="name" value="1"/>
   </properties>
-  <image source="../../../Downloads/New Piskel.png" width="32" height="32"/>
+  <image source="../../../Downloads/New Piskel (4).png" width="32" height="32"/>
  </tile>
  <tile id="3">
   <properties>
@@ -24,5 +24,23 @@
    <property name="name" value="0-0"/>
   </properties>
   <image source="../../../Downloads/New Piskel (3).png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="name" value="4"/>
+  </properties>
+  <image source="../../../Downloads/New Piskel (5).png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="name" value="3"/>
+  </properties>
+  <image source="../../../Downloads/New Piskel.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="name" value="2"/>
+  </properties>
+  <image source="../../../Downloads/New Piskel (1).png" width="32" height="32"/>
  </tile>
 </tileset>

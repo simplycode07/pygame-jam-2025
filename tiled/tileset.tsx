642b2fedc23097101bd910e32e94fd26fbb13175
simplycode07 <simplycode07@gmail.com>
2025-02-11 20:20:39 +0530

    changes to levels for teleportation to work

--- tiled/tileset.tsx
+++ tiled/tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="basky" tilewidth="32" tileheight="32" tilecount="2" columns="0">
+<tileset version="1.10" tiledversion="1.11.1" name="basky" tilewidth="32" tileheight="32" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
@@ -12,5 +12,11 @@
    <property name="name" value="-2"/>
   </properties>
   <image source="../assets/basky_32x32.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="name" value="0"/>
+  </properties>
+  <image source="../../../Downloads/New Piskel (2).png" width="32" height="32"/>
  </tile>
 </tileset>
